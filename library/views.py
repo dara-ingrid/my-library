1a796daa26ab48a3fb73adeff8e8e767497cff9e
Dara Ferreira <dara-ingrid@hotmail.com>
2023-04-19 15:42:17 -0300

    Configure static files

--- library/views.py
+++ library/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-        return render(request, 'galeria/index.html')
+        return render(request, 'library/index.html')
--- setup/settings.py
+++ setup/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'setup/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
