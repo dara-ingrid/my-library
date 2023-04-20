50f42851b66e68376f66425493db207f6b396d75
Dara Ferreira <dara-ingrid@hotmail.com>
2023-04-20 17:18:01 -0300

    access database data

--- library/views.py
+++ library/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
 
-def index(request):
+from library.models import Livraria
 
-    dados = {
-    1: {"titulo": "Código limpo: Habilidades práticas do Agile Software",
-        "autor": "Robert C. Martin"},
-    2: {"titulo": "Mais esperto que o Diabo: O mistério revelado da liberdade e do sucesso",
-        "autor": "Napoleon Hill"},
-    }
-    
-    return render(request, 'library/index.html', {"cards": dados})
+def index(request):
+    livros = Livraria.objects.all()
+    return render(request, 'library/index.html', {"cards": livros})
 
 def imagem(request):
     return render(request, 'library/imagem.html')
