25484fa58a296df4ce37a990b1cef69ce9a01e1f
Dara Ferreira <dara-ingrid@hotmail.com>
2023-04-20 15:56:59 -0300

    dynamic fields

--- library/views.py
+++ library/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'library/index.html')
+
+    dados = {
+    1: {"titulo": "Código limpo: Habilidades práticas do Agile Software",
+        "autor": "Robert C. Martin"},
+    2: {"titulo": "Mais esperto que o Diabo: O mistério revelado da liberdade e do sucesso",
+        "autor": "Napoleon Hill"},
+    }
+    
+    return render(request, 'library/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'library/imagem.html')
