e652b2277c2f2c59f36814cbb012e399df126c59
Dara Ferreira <dara-ingrid@hotmail.com>
2023-04-20 17:57:53 -0300

    send object image reference

--- library/views.py
+++ library/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from library.models import Livraria
 
@@ -6,5 +6,6 @@ def index(request):
     livros = Livraria.objects.all()
     return render(request, 'library/index.html', {"cards": livros})
 
-def imagem(request):
-    return render(request, 'library/imagem.html')
+def imagem(request, foto_id):
+    livro = get_object_or_404(Livraria, pk=foto_id)
+    return render(request, 'library/imagem.html', {"livro": livro})
