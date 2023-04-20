a07b55d05aad101ee7f76589c23b925d0a68fdc0
Dara Ferreira <dara-ingrid@hotmail.com>
2023-04-20 16:43:33 -0300

    Creating a Model in the Database

--- library/models.py
+++ library/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Livro(models.Model):
+    titulo = models.TextField(max_length=100, null=False, blank=False)
+    Autor = models.CharField(max_length=100, null=False, blank=False)
+    capa_livro = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f"Livro [titulo={self.titulo}]"
