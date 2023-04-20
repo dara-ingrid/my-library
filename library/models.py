23e04ef9dd0c64d90f0be77f93d059474b56e600
Dara Ferreira <dara-ingrid@hotmail.com>
2023-04-20 18:49:04 -0300

    including category with options

--- library/models.py
+++ library/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Livraria(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ("AUTOAJUDA", "Autoajuda"),
+        ("PROGRAMAÇÃO", "Programação"),
+        ("CULINÁRIA", "Culinária"),
+        ("FICÇÃO CIENTÍFICA", "Ficção Científica"),
+    ]
+
     titulo = models.TextField(max_length=100, null=False, blank=False)
     autor = models.CharField(max_length=100, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     capa_livro = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
