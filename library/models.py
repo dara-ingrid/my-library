4d21a5a0ed343aa60d50ad85056711015333dc97
Dara Ferreira <dara-ingrid@hotmail.com>
2023-04-23 18:33:20 -0300

    Add 'date of publication' field, and include filter by date

--- library/models.py
+++ library/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from datetime import datetime
+
 class Livraria(models.Model):
 
     OPCOES_CATEGORIA = [
@@ -14,6 +16,7 @@ class Livraria(models.Model):
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     capa_livro = models.CharField(max_length=100, null=False, blank=False)
     disponivel = models.BooleanField(default=True)
+    data_publicacao = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
         return f"Livro [titulo={self.titulo}]"
--- library/views.py
+++ library/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from library.models import Livraria
 
 def index(request):
-    livros = Livraria.objects.filter(disponivel=True)
+    livros = Livraria.objects.order_by("data_publicacao").filter(disponivel=True)
     return render(request, 'library/index.html', {"cards": livros})
 
 def imagem(request, foto_id):
