88bf63914971240ba8ab5935bd361103b16db61d
Dara Ferreira <dara-ingrid@hotmail.com>
2023-04-23 18:07:47 -0300

    add filter by Category, and pagination

--- library/admin.py
+++ library/admin.py
@@ -5,6 +5,8 @@ from library.models import Livraria
 class ListandoLivros(admin.ModelAdmin):
     list_display = ("id", "titulo", "autor")
     list_display_links = ("id", "titulo")
-    search_fields = ("titulo"),
+    search_fields = ("titulo", )
+    list_filter = ("categoria",)
+    list_per_page = 10
 
 admin.site.register(Livraria, ListandoLivros)
