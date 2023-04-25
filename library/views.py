982c92b7cc6dd5dd00743d9a265b46491bc19814
Dara Ferreira <dara-ingrid@hotmail.com>
2023-04-25 11:52:13 -0300

    Adds Search page resource  and fix some tag settings in css

--- library/views.py
+++ library/views.py
@@ -11,4 +11,11 @@ def imagem(request, foto_id):
     return render(request, 'library/imagem.html', {"livro": livro})
 
 def buscar(request):
-    return render(request, "library/buscar.html")
+    livros = Livraria.objects.order_by("data_publicacao").filter(disponivel=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            livros = livros.filter(titulo__icontains=nome_a_buscar)
+
+    return render(request, "library/buscar.html", {"cards": livros})
