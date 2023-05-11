5e115e360245475c4d295a4dc8ede8e9e8a7c2a6
Dara Ferreira <dara-ingrid@hotmail.com>
2023-05-11 16:02:07 -0300

    Create Login form

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from users.forms import LoginForms
+
 def login(request):
-    return render(request, "users/login.html")
+    form = LoginForms()
+    return render(request, "users/login.html", {"form": form})
 
 def cadastro(request):
     return render(request, "users/cadastro.html")
