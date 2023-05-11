b3f70a2f59b85a5c39ffd81db430f900d1acfc91
Dara Ferreira <dara-ingrid@hotmail.com>
2023-05-11 09:50:33 -0300

    Create urls and views of users

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from users.views import login, cadastro
 
 urlpatterns = [
-
+    path('login', login, name='login'),
+    path('cadastro', cadastro, name='cadastro'),
 ]
