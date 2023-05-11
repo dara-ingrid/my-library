32563059143abcf725d78b9f19c209f93b295116
Dara Ferreira <dara-ingrid@hotmail.com>
2023-05-11 09:13:34 -0300

    Creating a new app - Users

--- setup/urls.py
+++ setup/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('library.urls')),
+    path('', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
