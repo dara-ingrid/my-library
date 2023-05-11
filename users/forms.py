b6fd35abe7762ace439ed22dbbe0ff6d1bc8f08a
Dara Ferreira <dara-ingrid@hotmail.com>
2023-05-11 16:18:31 -0300

    Add style to login form

--- users/forms.py
+++ users/forms.py
@@ -4,11 +4,22 @@ class LoginForms(forms.Form):
     nome_login=forms.CharField(
         label="Nome de login",
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder" : "Ex: Maria Clara"                
+            }
+        )
     )
     senha=forms.CharField(
         label="Senha",
         required=True,
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder" : "Digite sua senha"
+            }
+        )
     )
