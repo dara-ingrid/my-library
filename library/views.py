5625cd4ea50c474d461cc28cdcf7e0f25efc39d2
Dara Ferreira <dara-ingrid@hotmail.com>
2023-05-15 16:10:21 -0300

    Includes permission layer only for logged in users

--- library/views.py
+++ library/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from library.models import Livraria
 
+from django.contrib import messages
+
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado!")
+        return redirect('login')
+
     livros = Livraria.objects.order_by("data_publicacao").filter(disponivel=True)
     return render(request, 'library/index.html', {"cards": livros})
 
@@ -11,6 +17,10 @@ def imagem(request, foto_id):
     return render(request, 'library/imagem.html', {"livro": livro})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado!")
+        return redirect('login')
+
     livros = Livraria.objects.order_by("data_publicacao").filter(disponivel=True)
 
     if "buscar" in request.GET:
