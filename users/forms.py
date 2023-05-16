4af74a87b681b0b17871438b88a6fbeb586a61d6
Dara Ferreira <dara-ingrid@hotmail.com>
2023-05-15 23:25:39 -0300

    refactor code

--- users/forms.py
+++ users/forms.py
@@ -8,7 +8,7 @@ class LoginForms(forms.Form):
         widget=forms.TextInput(
             attrs={
                 "class": "form-control",
-                "placeholder" : "Ex: Maria Clara"                
+                "placeholder" : "Ex: mariaclara"                
             }
         )
     )
@@ -32,7 +32,7 @@ class CadastroForms(forms.Form):
         widget=forms.TextInput(
             attrs={
                 "class": "form-control",
-                "placeholder" : "Ex: Maria Clara"                
+                "placeholder" : "Ex: mariaclara"                
             }
         )
     )
