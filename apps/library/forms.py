367b5c533136b0bc7eeeeea8cf2603cac2fa6db7
Dara Ferreira <dara-ingrid@hotmail.com>
2023-05-16 11:52:14 -0300

    Django form for the new image feature, based on an existing model

---
+++ apps/library/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from apps.library.models import Livraria
+
+class LivrariaForms(forms.ModelForm):
+    class Meta:
+        model = Livraria
+        exclude = ['disponivel',]
+
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control',}),
+            'autor': forms.TextInput(attrs={'class': 'form-control',}),
+            'categoria': forms.Select(attrs={'class': 'form-control',}),
+            'capa_livro': forms.FileInput(attrs={'class': 'form-control',}),
+            'data_publicacao': forms.DateInput(
+                format='%d/%m/%a',
+                attrs={
+                    'type' : 'date',
+                    'class': 'form-control',
+                }
+            ),
+             'usuario': forms.Select(attrs={'class': 'form-control',}),
+            
+        }
